db9862bdc67169dd97fbba23caf3ae59f4be659b
KashanMehmood <kashan.mehmood13@gmail.com>
2020-04-15 20:32:17 +0500

    file system with sync, async

--- Index.js
+++ Index.js
@@ -14,24 +14,52 @@
 // console.log("my name is %s", name);
 // console.log(`my name is ${name}`);
 
+// __________________________________________________________________________
+const fs = require('fs');
+// console.log(fs);
 
-const http = require('http');
-const server = http.createServer((request,reponse) => {
+// unlink, unlinkSync Methods
 
-    reponse.setHeader('content-type','text/html');
+// const data = fs.readFileSync('ReadME.md');
+// console.log(data.toString());
+// console.log("File Readed Successfully");
+// Output
+// Synchronous call back
+// it will print File content first then log message.
+
+
+// __________________________________________________________________________
+// const fs = require('fs');
+// fs.readFile('ReadME.md', function(error, response) {
+//     console.log(response.toString());
+//     // res.writeHead(200, {'Content-Type': 'text/html'});
+//     // res.write(data);
+//     // res.end();
+// });
+// console.log("File closed");
+
+// Output
+// Asynchronous call back
+// it will print File closed first then file content.
+
+// __________________________________________________________________________
+// const http = require('http');
+// const server = http.createServer((request,reponse) => {
+
+//     reponse.setHeader('content-type','text/html');
     
-    if(request.url == '/'){
-        reponse.statusCode = 200;
-        reponse.write('Hello NodeJS');
-        reponse.write("<div>Current Url " + request.url + "</div>");
-    }else if(request.url == '/hello'){
-        reponse.write("hello url");
-        reponse.write("<div>Current Url " + request.url + "</div>");
-    }else{
-        reponse.write("wrong url");
-    }
+//     if(request.url == '/'){
+//         reponse.statusCode = 200;
+//         reponse.write('Hello NodeJS');
+//         reponse.write("<div>Current Url " + request.url + "</div>");
+//     }else if(request.url == '/hello'){
+//         reponse.write("hello url");
+//         reponse.write("<div>Current Url " + request.url + "</div>");
+//     }else{
+//         reponse.write("wrong url");
+//     }
 
-    reponse.end();
-});
+//     reponse.end();
+// });
 
-server.listen(3000);
+// server.listen(3000);
