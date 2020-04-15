81e68fe73348bdf5ec5f714b7f5a39f2eaa7d08d
KashanMehmood <kashan.mehmood13@gmail.com>
2020-04-15 16:22:12 +0500

    node url

--- Index.js
+++ Index.js
@@ -8,8 +8,30 @@
 // console.log(__dirname);
 // console.log(__filename);
 
-const name = "kashan";
-const age = 20;
+// const name = "kashan";
+// const age = 20;
 
-console.log("my name is %s", name);
-console.log(`my name is ${name}`);
+// console.log("my name is %s", name);
+// console.log(`my name is ${name}`);
+
+
+const http = require('http');
+const server = http.createServer((request,reponse) => {
+
+    reponse.setHeader('content-type','text/html');
+    
+    if(request.url == '/'){
+        reponse.statusCode = 200;
+        reponse.write('Hello NodeJS');
+        reponse.write("<div>Current Url " + request.url + "</div>");
+    }else if(request.url == '/hello'){
+        reponse.write("hello url");
+        reponse.write("<div>Current Url " + request.url + "</div>");
+    }else{
+        reponse.write("wrong url");
+    }
+
+    reponse.end();
+});
+
+server.listen(3000);
